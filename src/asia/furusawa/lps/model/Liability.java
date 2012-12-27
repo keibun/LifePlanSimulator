41bf32a10509a00f2fa986fb55f45e02615d58d2
hiro <hiro@192.168.0.3>
2012-12-28 01:27:43 +0900

    add sample chart.

---
+++ src/asia/furusawa/lps/model/Liability.java
@@ -0,0 +1,27 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package asia.furusawa.lps.model;
+
+/**
+ *
+ * @author hiro
+ */
+public class Liability {
+    private int value;
+
+    /**
+     * @return the value
+     */
+    public int getValue() {
+        return value;
+    }
+
+    /**
+     * @param value the value to set
+     */
+    public void setValue(int value) {
+        this.value = value;
+    }
+}
--- src/lifeplansimulatorprototype/LifePlanSimulatorPrototype.java
+++ src/lifeplansimulatorprototype/LifePlanSimulatorPrototype.java
@@ -7,9 +7,13 @@ package lifeplansimulatorprototype;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.scene.Group;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
+import javafx.scene.chart.CategoryAxis;
+import javafx.scene.chart.LineChart;
+import javafx.scene.chart.NumberAxis;
+import javafx.scene.chart.XYChart;
+
 import javafx.stage.Stage;
 
 /**
@@ -17,26 +21,40 @@ import javafx.stage.Stage;
  * @author furusawa.hirofumi
  */
 public class LifePlanSimulatorPrototype extends Application {
-    
+
     @Override
     public void start(Stage primaryStage) {
-        Button btn = new Button();
-        btn.setText("Say 'Hello World'");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
-            }
-        });
-        
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
-        
-        Scene scene = new Scene(root, 300, 250);
-        
-        primaryStage.setTitle("Hello World!");
+
+
+        Group root = new Group();
+
+        Scene scene = new Scene(root, 1024, 768);
+        root.getChildren().add(createChart());
+
+        primaryStage.setTitle("Chart Test");
         primaryStage.setScene(scene);
         primaryStage.show();
+    }
+    private static final String[] CATEGORIES = {"2013/1","2013/2","2013/3","2013/4","2013/5","2013/6"};
+    protected LineChart<String, Number> createChart() {
+        final CategoryAxis xAxis = new CategoryAxis();
+        final NumberAxis yAxis = new NumberAxis();
+        final LineChart<String, Number> lc = new LineChart<>(xAxis, yAxis);
+        // setup chart
+        lc.setTitle("LineChart with Category Axis");
+        xAxis.setLabel("時期");
+        yAxis.setLabel("円");
+        // add starting data
+        XYChart.Series<String, Number> series = new XYChart.Series<>();
+        series.setName("Data Series 1");
+        series.getData().add(new XYChart.Data<String, Number>(CATEGORIES[0], 50d));
+        series.getData().add(new XYChart.Data<String, Number>(CATEGORIES[1], 80d));
+        series.getData().add(new XYChart.Data<String, Number>(CATEGORIES[2], 90d));
+        series.getData().add(new XYChart.Data<String, Number>(CATEGORIES[3], 30d));
+        series.getData().add(new XYChart.Data<String, Number>(CATEGORIES[4], 122d));
+        series.getData().add(new XYChart.Data<String, Number>(CATEGORIES[5], 10d));
+        lc.getData().add(series);
+        return lc;
     }
 
     /**
